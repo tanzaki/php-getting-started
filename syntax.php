3777e31fda97cc709f2519a8da6d07fd99716180
haonx <qtvhao@gmail.com>
2017-07-25 22:25:49 +0700

    Comments in PHP
    https://www.w3schools.com/php/php_syntax.asp

--- syntax.php
+++ syntax.php
@@ -13,3 +13,31 @@
 
     Below, we have an example of a simple PHP file, with a PHP script that uses a built-in PHP function "echo" to output the text "Hello World!" on a web page:
 </div>
+<div>
+    Comments in PHP
+    A comment in PHP code is a line that is not read/executed as part of the program. Its only purpose is to be read by someone who is looking at the code.
+
+    Comments can be used to:
+
+    Let others understand what you are doing
+    Remind yourself of what you did - Most programmers have experienced coming back to their own work a year or two later and having to re-figure out what they did. Comments can remind you of what you were thinking when you wrote the code
+    PHP supports several ways of commenting:
+
+    Example
+
+	<?php
+	// This is a single-line comment
+
+	# This is also a single-line comment
+
+	/*
+	This is a multiple-lines comment block
+	that spans over multiple
+	lines
+	*/
+
+	// You can also use comments to leave out parts of a code line
+	$x = 5 /* + 15 */ + 5;
+	echo $x;
+	?>
+</div>
