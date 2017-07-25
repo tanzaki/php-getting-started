61515ed1488aa3ac8f907cc4e0c668fcb3f327ee
haonx <qtvhao@gmail.com>
2017-07-25 22:22:54 +0700

    Basic PHP Syntax
    https://www.w3schools.com/php/php_syntax.asp

---
+++ syntax.php
@@ -0,0 +1,15 @@
+<div>
+    Basic PHP Syntax
+    A PHP script can be placed anywhere in the document.
+
+    A PHP script starts with &lt;?php and ends with ?>:
+
+	<?php
+	// PHP code goes here
+	?>
+    The default file extension for PHP files is ".php".
+
+    A PHP file normally contains HTML tags, and some PHP scripting code.
+
+    Below, we have an example of a simple PHP file, with a PHP script that uses a built-in PHP function "echo" to output the text "Hello World!" on a web page:
+</div>
