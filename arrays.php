b046cf09bf5335f4b5ed7aa98b444ae062be0124
haonx <qtvhao@gmail.com>
2017-07-25 23:51:15 +0700

    PHP 5 Arrays
    https://www.w3schools.com/php/php_arrays.asp

---
+++ arrays.php
@@ -0,0 +1,6 @@
+An array stores multiple values in one single variable:
+
+Example
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
