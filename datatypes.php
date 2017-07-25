22a97ee997f69887abe7ce15d546c131fda77e9a
haonx <qtvhao@gmail.com>
2017-07-25 22:41:02 +0700

    PHP 5 Data Types
    https://www.w3schools.com/php/php_datatypes.asp

---
+++ datatypes.php
@@ -0,0 +1,31 @@
+PHP Data Types
+Variables can store data of different types, and different data types can do different things.
+<div></div>
+PHP supports the following data types:
+
+String
+Integer
+Float
+Boolean
+Array
+Object
+NULL
+Resource
+<h2>PHP String</h2>
+A string is a sequence of characters, like "Hello world!".
+
+A string can be any text inside quotes. You can use single or double quotes:
+
+<div>
+	<div>
+		Example
+	</div>
+	<?php
+	$x = "Hello world!";
+	$y = 'Hello world!';
+
+	echo $x;
+	echo "<br>";
+	echo $y;
+	?>
+</div>
