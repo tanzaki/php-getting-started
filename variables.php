c30105185f4499d85feb7843693814f788ed6b19
haonx <qtvhao@gmail.com>
2017-07-25 22:31:25 +0700

    Creating (Declaring) PHP Variables
    https://www.w3schools.com/php/php_variables.asp

---
+++ variables.php
@@ -0,0 +1,14 @@
+Creating (Declaring) PHP Variables
+In PHP, a variable starts with the $ sign, followed by the name of the variable:
+<div>
+    Think of variables as containers for storing data.
+</div>
+<div>
+Example
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+echo $txt;
+?>
+</div>
