9784bbeb462a854d21ba6f7c8345c6e5ec1e13da
haonx <qtvhao@gmail.com>
2017-07-25 22:15:45 +0700

    What is PHP?
    https://www.w3schools.com/php/php_intro.asp

---
+++ intro.php
@@ -0,0 +1,18 @@
+What is PHP?
+PHP is an acronym for "PHP: Hypertext Preprocessor"
+PHP is a widely-used, open source scripting language
+PHP scripts are executed on the server
+PHP is free to download and use
+PHP is an amazing and popular language!
+
+> It is powerful enough to be at the core of the biggest blogging system on the web (WordPress)!
+> It is deep enough to run the largest social network (Facebook)!
+> It is also easy enough to be a beginner's first server side language!
+
+What is a PHP File?
+PHP files can contain text, HTML, CSS, JavaScript, and PHP code
+<?php
+echo '<div></div>';
+echo '<div>PHP code are executed on the server, and the result is returned to the browser as plain HTML</div>'
+?>
+PHP files have extension ".php"
