9fa8208fa22ed23d8e9bfb42d8ec63e83bf53149
haonx <qtvhao@gmail.com>
2017-07-25 23:53:33 +0700

    PHP 5 Arrays
    https://www.w3schools.com/php/php_arrays.asp

--- arrays.php
+++ arrays.php
@@ -4,3 +4,19 @@ Example
 <?php
 $cars = array("Volvo", "BMW", "Toyota");
 echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+?>
+<h2>What is an Array?</h2>
+An array is a special variable, which can hold more than one value at a time.
+
+If you have a list of items (a list of car names, for example), storing the cars in single variables could look like this:
+
+<pre>
+    $cars1 = "Volvo";
+    $cars2 = "BMW";
+    $cars3 = "Toyota";
+</pre>
+However, what if you want to loop through the cars and find a specific one? And what if you had not 3 cars, but 300?
+
+The solution is to create an array!
+
+An array can hold many values under a single name, and you can access the values by referring to an index number.
