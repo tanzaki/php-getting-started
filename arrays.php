6c798d941e56137819df4bc9d9d8182aaecdf57a
haonx <qtvhao@gmail.com>
2017-07-25 23:57:55 +0700

    PHP Indexed Arrays
    https://www.w3schools.com/php/php_arrays.asp

--- arrays.php
+++ arrays.php
@@ -20,3 +20,28 @@ However, what if you want to loop through the cars and find a specific one? And 
 The solution is to create an array!
 
 An array can hold many values under a single name, and you can access the values by referring to an index number.
+<div>
+    <h2>PHP Indexed Arrays</h2>
+    There are two ways to create indexed arrays:
+
+    The index can be assigned automatically (index always starts at 0), like this:
+
+    <pre>
+        $cars = array("Volvo", "BMW", "Toyota");
+    </pre>
+    or the index can be assigned manually:
+
+    <pre>
+        $cars[0] = "Volvo";
+        $cars[1] = "BMW";
+        $cars[2] = "Toyota";
+    </pre>
+    The following example creates an indexed array named $cars, assigns three elements to it, and then prints a text containing the array values:
+</div>
+<div>
+    Example
+	<?php
+	$cars = array("Volvo", "BMW", "Toyota");
+	echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+	?>
+</div>
