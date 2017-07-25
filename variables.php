91ce0b444f5830eebd18ed5acf550699e3aebd91
haonx <qtvhao@gmail.com>
2017-07-25 22:36:37 +0700

    Output Variables
    https://www.w3schools.com/php/php_variables.asp

--- variables.php
+++ variables.php
@@ -12,3 +12,34 @@ $y = 10.5;
 echo $txt;
 ?>
 </div>
+<h2>Output Variables</h2>
+The PHP echo statement is often used to output data to the screen.
+
+The following example will show how to output text and a variable:
+
+<div>
+    Example
+    <?php
+    $txt = "W3Schools.com";
+    echo "I love $txt!";
+    ?>
+</div>
+The following example will produce the same output as the example above:
+
+<div>
+    Example
+    <?php
+    $txt = "W3Schools.com";
+    echo "I love " . $txt . "!";
+    ?>
+</div>
+Run example »
+The following example will output the sum of two variables:
+<div>
+    Example
+	<?php
+	$x = 5;
+	$y = 4;
+	echo $x + $y;
+	?>
+</div>
