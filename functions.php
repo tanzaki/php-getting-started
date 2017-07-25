e1965a5bc56ce5c23c8d2532bf180b67c524a035
haonx <qtvhao@gmail.com>
2017-07-25 23:44:52 +0700

    The real power of PHP comes from its functions; it has more than 1000 built-in functions.
    https://www.w3schools.com/php/php_string.asp

---
+++ functions.php
@@ -0,0 +1,12 @@
+The real power of PHP comes from its functions; it has more than 1000 built-in functions.
+<h2>Replace Text Within a String</h2>
+The PHP str_replace() function replaces some characters with some other characters in a string.
+
+The example below replaces the text "world" with "Dolly":
+
+<div>
+	Example
+	<?php
+	echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+	?>
+</div>
